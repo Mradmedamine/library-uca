8d1d0be2761b882b3d69f54db7808b4b2edff358
MedAmine <MedAmine@PC-MohamedAmine.home>
2017-07-30 16:07:43 +0200

    recordsTable & cleaning

--- src/main/java/org/library/uca/domain/Book.java
+++ src/main/java/org/library/uca/domain/Book.java
@@ -1,11 +1,6 @@
 package org.library.uca.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
