6fec98876ccbd71a42092be0ef171d92483ea3b3
Mradmedamine <mradmohamedamine@gmail.com>
2017-08-12 20:24:30 +0200

    @Param

--- src/main/java/org/library/uca/repository/RecordRepository.java
+++ src/main/java/org/library/uca/repository/RecordRepository.java
@@ -7,10 +7,13 @@ import org.library.uca.model.domain.RecordType;
 import org.library.uca.model.domain.entity.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
-	@Query("select u from Record u where upper(u.description) like ?1 and u.status in ?2 and u.type in ?3")
-	List<Record> findByCriteria(String description, List<RecordStatus> statusList, List<RecordType> typeList);
+	@Query("select u from Record u where upper(u.description) like :description and u.status in :statusList and u.type in :typeList")
+	List<Record> findByCriteria(@Param("description") String description,
+								@Param("statusList") List<RecordStatus> statusList,
+								@Param("typeList") List<RecordType> typeList);
 
 }
